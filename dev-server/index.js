69a770f3f8fd7789b48fade2962bd87cb1db07b4
xxxDNxxx <beerurban@gmail.com>
2020-04-17 11:20:21 +0700

    Use-Expressjs-Middleware-and-CORS-configuration

--- dev-server/index.js
+++ dev-server/index.js
@@ -1,8 +1,18 @@
 import express from 'express'
 const app = express()
 import { registerRoutes } from './routes'
+import { setEnvironment } from './config/env'
 
-registerRoutes(app);
+setEnvironment(app)
+registerRoutes(app)
 
-app.get('/', (req, res) => res.send("hello"))
-app.listen(3000, () => console.log("listen on 3000"))
+app.get('/', (req, res) => {
+    if (process.env.NODE_ENV !== 'production') {
+        return res.send('Running server in Development mode.')
+    } else {
+        return res.sendFile('index.html', { root: __dirname + '/../dist/' })
+    }
+})
+app.listen(3000, () => {
+    console.log('app listen on port 3000' + process.env.NODE_ENV + 'mode.')
+})
--- prod-server/index.js
+++ prod-server/index.js
@@ -6,16 +6,23 @@ var _express2 = _interopRequireDefault(_express);
 
 var _routes = require('./routes');
 
+var _env = require('./config/env');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
 
 
+(0, _env.setEnvironment)(app);
 (0, _routes.registerRoutes)(app);
 
 app.get('/', function (req, res) {
-  return res.send("hello");
+    if (process.env.NODE_ENV !== 'production') {
+        return res.send('Running server in Development mode.');
+    } else {
+        return res.sendFile('index.html', { root: __dirname + '/../dist/' });
+    }
 });
 app.listen(3000, function () {
-  return console.log("listen on 3000");
+    console.log('app listen on port 3000' + process.env.NODE_ENV + 'mode.');
 });
